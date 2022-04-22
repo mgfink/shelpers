4db039956923a60585ff43e24209b6591b6951ea
ABGDDZZ <ABGDDZZ@mmm.com>
2022-04-22 11:30:21 -0500

    Updates

--- Mac/pr-report.sh
+++ Mac/pr-report.sh
@@ -1,4 +1,4 @@
 TMPFILE=$(mktemp)
 echo "TMPFILE=$TMPFILE"
-find ~/src -type d -depth 1 -name "aws-voyager*" -print -exec bash -c 'cd {} && gh pr list && echo ---------' \; | tee $TMPFILE
+find ~/src -type d -depth 1 \( -name "aws-voyager*" -or -name "voyager-request-ui" \) -print -exec bash -c 'cd {} && /usr/local/bin/gh pr list && echo ---------' \; | tee $TMPFILE
 mail -s "Dailiy PR Report" michael.fink@analysts.com < $TMPFILE
